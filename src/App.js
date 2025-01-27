b1952291d87b82a5b91233d9de26d4e1c6907757
Bundele-Durwangsingh <bundeledurwang@gmail.com>
2025-01-27 17:57:11 +0530

    Modify : Completed css

--- src/App.js
+++ src/App.js
@@ -4,27 +4,34 @@ import TaskInput from './components/TaskInput';
 import TaskList from './components/TaskList';
 
 function App() {
-  const [list,setList]=useState([]);
-  let taskList=(inputText)=>{
-    setList([...list,inputText]);
-  }
-  const deleteTask =  (key)=> {
-    let newList =[...list];
-    newList.splice(key,1)
-    setList([...newList])
-  }
+  const [list, setList] = useState([]);
+
+  const taskList = (inputText) => {
+    setList([...list, inputText]);
+  };
+
+  const deleteTask = (index) => {
+    setList(list.filter((_, i) => i !== index));
+  };
+
   return (
     <div className="App">
-      <h1 className='heading'>Todo App</h1>
-      <TaskInput taskList={taskList}/>
-      
-      <hr/>
-      {list.map((task,i) => {
-        return( 
-        <TaskList key = {i} item ={task} deleteTask={deleteTask} index ={i}/> 
-        
-      )
-      })}
+      <div className="todo-container">
+        <h1 className="heading">Todo App</h1>
+        <TaskInput taskList={taskList} />
+        <hr />
+        <div className="todo-list">
+          {list.map((task, i) => (
+            <TaskList key={i} item={task} deleteTask={deleteTask} index={i} />
+          ))}
+        </div>
+        <p>You have {list.length} pending tasks</p>
+        {list.length > 0 && (
+          <button className="clear-btn" onClick={() => setList([])}>
+            Clear All
+          </button>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -12,7 +12,6 @@ function TaskList(props) {
                 </i>
                 </span>
     </li>
-
     )
 }
 
