82fbc55c743571624648429d2d7855086043f465
Bundele-Durwangsingh <bundeledurwang@gmail.com>
2025-01-28 18:28:09 +0530

    Modify : Added search box and made responsive

---
+++ src/hooks/useTaskList.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const useTaskList = () => {
+  const [list, setList] = useState([]);
+
+  const addTask = (taskText) => {
+    const newTask = {
+      id: uuidv4(), 
+      text: taskText,
+    };
+    setList((prevList) => [...prevList, newTask]);
+  };
+
+  const deleteTask = (taskId) => {
+    setList((prevList) => prevList.filter((task) => task.id !== taskId));
+  };
+
+  const clearTasks = () => {
+    setList([]);
+  };
+
+  return {
+    list,
+    addTask,
+    deleteTask,
+    clearTasks,
+  };
+};
+
+export default useTaskList;
